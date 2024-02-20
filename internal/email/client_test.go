b9db5cdb285beeccdbe9b4df271bcd4c3410993c
adrian2monk <adrian2monk@gmail.com>
2024-02-20 12:47:40 -0600

    Add TestClient_SendMail_Summary

--- internal/email/client_test.go
+++ internal/email/client_test.go
@@ -1,27 +1,14 @@
 package email
 
 import (
-	"crypto/x509"
-	"crypto/x509/pkix"
 	"io"
-	"net"
 	"strings"
 	"testing"
 
+	"github.com/ahbarrios/stori-txn-test/internal/email/template"
+	"github.com/ahbarrios/stori-txn-test/internal/tx/agg"
 	"github.com/emersion/go-sasl"
 )
-
-var csr = x509.CertificateRequest{
-	Subject: pkix.Name{
-		Country:            []string{"MX"},
-		Organization:       []string{"Stori"},
-		OrganizationalUnit: []string{"Security"},
-		Locality:           []string{"CDMX"},
-		CommonName:         "storicard.com",
-	},
-	DNSNames:    []string{"*.storicard.com", "localhost"},
-	IPAddresses: []net.IP{net.IPv4(127, 0, 0, 1)},
-}
 
 func TestClient_SendMail(t *testing.T) {
 	a := sasl.NewPlainClient("", "username", "password")
@@ -33,6 +20,38 @@ func TestClient_SendMail(t *testing.T) {
 		"This is the email body.\r\n")
 	err := c.SendMail("sender@localhost", []string{"to@example.com"}, func() (io.Reader, error) {
 		return m, nil
+	})
+	if err != nil {
+		t.Fatalf("SendMail() error = %v", err)
+	}
+}
+
+func TestClient_SendMail_Summary(t *testing.T) {
+	a := sasl.NewPlainClient("", "username", "password")
+	c := NewClient(testServerAddr, a)
+
+	err := c.SendMail("sender@localhost", []string{"to@example.com"}, func() (io.Reader, error) {
+		var (
+			c agg.AvgCredit
+			d agg.AvgDebit
+		)
+		r, err := template.NewSummaryBody(agg.Balance(39.6), c, d, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		// Read all bytes from body email
+		br, err := io.ReadAll(r)
+		if err != nil {
+			return nil, err
+		}
+
+		// Compound the email body with RFC 822 compatible format
+		m := strings.NewReader("To: to@example.com\r\n" +
+			"Subject: testing Client.SendMail Summary Email!\r\n" +
+			"\r\n" +
+			string(br) + "\r\n")
+		return m, err
 	})
 	if err != nil {
 		t.Fatalf("SendMail() error = %v", err)
